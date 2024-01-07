be9280e3293730d500a8281c5a83b92e96c4aea6
Raziel Rodrigues <razielx3@live.com>
2024-01-07 23:42:18 +0000

    feat 2

--- app/src/Controller/VynilController.php
+++ app/src/Controller/VynilController.php
@@ -1,14 +1,28 @@
 <?php
 
+namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class VynilController extends AbstractController
+class VynilController
 {
     #[Route("/")]
     public function homepage()
     {
         return new Response("Welcome to my fancy vynil store!");
+    }
+
+    #[Route("/browse/{slug}")]
+    public function browse(string $slug = null)
+    {
+        $title = "All genres";
+
+        if ($slug) {
+            $title = str_replace('-', ' ', $slug);
+        }
+
+        return new Response($title);
     }
 }
